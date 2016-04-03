2d3262ac01131b3428368785943c12517714548d
Paul Chun <me@paulchun.com>
2016-04-03 10:24:43 -0400

    Half implement testing function and add a test test.

--- src/test_engine/main.cpp
+++ src/test_engine/main.cpp
@@ -4,17 +4,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 #include "global_lock_ll/global_lock_linked_list.h"
 
 DEFINE_string(testfile, "tests/hello.txt", "Test files to run.");
 
-int run_tests(std::string testfile) {
+std::vector<std::string> split( const std::string &str, const char &delim ) {
+  std::string::const_iterator curr = str.begin();
+  std::vector<std::string> tokens;
+
+  while (curr != str.end()) {
+    std::string::const_iterator temp = find(curr, str.end(), delim);
+    if (curr != str.end())
+      tokens.push_back(std::string(curr, temp));
+    curr = temp;
+    while ((curr != str.end()) && (*curr == delim))
+      curr++;
+  }
+
+  return tokens;
+}
+
+bool process_testline(std::string testline, GlobalLockLinkedList& ll) {
+  DLOG(INFO) << "Testing line: \x1b[36m" << testline << "\x1b[0m";
+  bool success = false;
+
+  std::vector<std::string> tokens = split(testline, ' ');
+
+  // TODO(paul) Add a way to parse test files here.
+
+  // Print test results
+  DLOG(INFO) << "Test complete. "
+             << "Results: \x1b[" 
+             << (success ? "32msuccess" : "31mfailed")
+             << "\x1b[0m";
+
+  return success;
+}
+
+void run_tests(std::string testfile) {
   DLOG(INFO) << "Starting tests in " << testfile << "...";
   bool all_test_success = false;
-
   GlobalLockLinkedList ll = GlobalLockLinkedList();
+
+  std::ifstream infile;
+  infile.open(testfile);
+  std::string testline;
+
+  while (!infile.eof()) {
+    getline(infile, testline);
+    all_test_success &= process_testline(testline, ll);
+  }
+  infile.close();
+
   DLOG(INFO) << "Inserting 'Hello'...";
   int id = ll.insert("Hello");
   DLOG(INFO) << "Node ID: " << id;
@@ -36,7 +84,6 @@ int run_tests(std::string testfile) {
   DLOG(INFO) << "Successfully removed: " << removed_it;
 
   DLOG_IF(INFO, all_test_success) << "All tests ran successfully!";
-  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -52,5 +99,7 @@ int main(int argc, char *argv[]) {
 
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  return run_tests(FLAGS_testfile);
+  run_tests(FLAGS_testfile);
+
+  return 0;
 }
