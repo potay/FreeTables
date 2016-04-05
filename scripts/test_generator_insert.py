fedef82434f12a006e22705d1d010b31c616520c
Paul Chun <me@paulchun.com>
2016-04-05 15:11:51 -0400

    Add a test generator python script

---
+++ scripts/test_generator_insert.py
@@ -0,0 +1,21 @@
+import random
+
+n = 100
+count = 0
+difference = 0
+keys = []
+output = ""
+for i in xrange(n):
+  if (random.random() < 0.4):
+    random.shuffle(keys)
+    for j in xrange(random.randint(0,len(keys))):
+      key = keys.pop()
+      output += "remove %d %d\n" % (key, len(keys))
+  else:
+    chosen_key = random.randint(1, n)
+    while (chosen_key in keys):
+      chosen_key = random.randint(1, n)
+    keys.append(chosen_key)
+    output +=  "insert %d %d %d\n" % (chosen_key, i, len(keys))
+
+print output
